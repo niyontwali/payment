c3a029af72ea78010c118e7355f55c462c90063b
John Niyontwali <john.ni0006@gmail.com>
2023-04-17 14:04:48 +0200

    payment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    console.log("element", script);
+    script.src =
+      "https://ap-gateway.mastercard.com/static/checkout/checkout.min.js";
+    script.setAttribute("data-error", "errorCallback");
+    script.setAttribute("data-cancel", "cancelCallback");
+    script.setAttribute("data-complete", "completeCallback");
+    script.async = true;
+
+    // function errorCallback(error) {
+    //   // When an error is encountered.
+    //   console.log(JSON.stringify(error));
+    // }
+    // function cancelCallback() {
+    //   // When the payer cancels the payment interaction
+    //   console.log("Payment cancelled");
+    // }
+    // function completeCallback(resultIndicator) {
+    //   // When the payer completes the payment interaction.
+    //   console.log("Payment complete");
+    //   console.log(resultIndicator);
+    //   // compaire the resultIndicator to successIndicator value, should be similar for a successful transaction.
+    //   // you can redirect your client to a success page at this point
+    // }
+
+    script.addEventListener("load", () => {
+      console.log("here");
+      window.Checkout.configure({
+        session: {
+          id: "ESSION0002014873411I57302725H0",
+        },
+      });
+    });
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  const handleEmbeddedPage = () => {
+    window.Checkout.showEmbeddedPage("#embed-target");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="embed-target"> </div>
+      <button onClick={handleEmbeddedPage}>
+        Pay with Embedded Page
+      </button>
     </div>
   );
 }
